82183dfb19acbb066cd7d0262d2ab679906c066f
Aarti Kushwaha ✨ <77312640+AartiKushwaha@users.noreply.github.com>
2023-09-29 21:34:24 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,162 @@
+var leetcode = "https://leetcode.com/Aarti_Kushwaha/";
+var twitter = "https://twitter.com/__rt_kush____";
+var linkedin = "https://www.linkedin.com/in/iam-aarti-kushwaha/";
+var instagram = "https://www.instagram.com/__rt_kush____/";
+var github = "https://github.com/AartiKushwaha";
+var sudo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
+var email = "mailto:rtkush1211@gmail.com";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hey, I'm Aarti!👋</span>",
+  "<br>",
+  "<li>🔭 Upcoming Tech Analyst Intern at Morgan Stanley,</li>",
+  `<li>🌱 currently in my final year pursuing a Bachelor's degree <br>
+  in Computer Science and Engineering at Indira Gandhi Delhi Technical University for Women.</li>
+  `,
+  `<li><pre class="whitespace-pre-wrap">👯 My passion for technology has driven me to explore various domains,
+  with a particular interest in Data Structures and Algorithms,
+  Web development, UI/UX designing, and most recently, Web 3.0.
+  I am committed to leveraging my skills and knowledge to make
+  meaningful contributions to the industry and achieve professional growth.
+   *Try using a social command.`,
+  `<li><pre class="whitespace-pre-wrap">👨‍💻 Check out my github profile to view my projects
+   *Use the github command to take a look at my GitHub profile.`,
+  `<li>📫 Contact me at <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=rtkush1211@gmail.com">rtkush1211@gmail.com</a>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'leetcode        <a href="' +
+    leetcode +
+    '" target="blank">leetcode.com/Aarti_Kushwaha' +
+    "</a>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="blank">twitter.com/__rt_kush____' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="blank">linkedin/iam-aarti-kushwaha' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="blank">instagram/__rt_kush____' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="blank">github/AartiKushwaha' +
+    "</a>",
+
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  `<a href="https://github.com/AartiKushwaha/blog-website/" target="blank"><span class='underline'>Petite Perspectives</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">Fully functional blog website with comprehensive features for user-generated content,
+   incorporating login and registration functionalities.
+
+
+Built With
+
+    *Next.js
+    *Tailwind CSS
+    *Programmable Search Engine
+    *Yarn
+`,
+
+  "<br>",
+
+  `<a href="https://monkedao.aartikushwaha.repl.co/" target="blank"><span class='underline'>Monke DAO</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">Decentralized Autonomous Organization utilizing membership NFTs,
+   allowing users to participate in voting on organizational proposals.
+
+Project is live here <a href="https://monkedao.aartikushwaha.repl.co/" target="blank">https://monkedao.aartikushwaha.repl.co/</a>
+
+Built With
+
+    *Next.js
+    *Tailwind CSS
+    *Google Sheets api
+    *IFTTT
+    *Yarn
+`,
+
+  "<br>",
+
+  `<a href="https://github.com/AartiKushwaha/RealTime-Chat_Application" target="blank"><span class='underline'>Whats Up</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">Real-time and highly responsive chat application
+   featuring multiple chat rooms to facilitate seamless communication between users..
+
+Project is live here <a href="https://realtime-chatapplication-production.up.railway.app/" target="blank">https://realtime-chatapplication-production.up.railway.app/</a>
+
+Built With
+
+    *Next.js
+    *Tailwind CSS
+    *Google Sheets api
+    *IFTTT
+    *Yarn
+`,
+
+  "<br>",
+
+  `<a href="https://github.com/AartiKushwaha/BuddyCanSIR" target="blank"><span class='underline'>BuddyCanSIR</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">Web portal for a non-governmental organization (NGO) aimed at delivering targeted
+   assistance to cancer survivors based on their specific needs and request.
+
+Built With
+
+    *Frontend : HTML, CSS, JS, Bootstrap, React
+    *Backend : NodeJs, ExpressJs
+    *Database : MongoDb
+    *API/Clouds : Google Translate, Cloudinary, Twilio, MongoDb Atlas, EmailJs
+    *Other : Git - Version Control, Visual Studio Code
+`,
+
+  "<br>",
+
+  "<br>",
+
+  `<a href="https://github.com/AartiKushwaha/PGLife" target="blank"><span class='underline'>PGlife</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">A portal which offers a streamlined platform for students to select appropriate PG accommodations
+   in various cities based on their specific preferences.
+
+Built With
+
+    *Frontend : HTML, CSS, JS, 
+    *Other : Git - Version Control, Visual Studio Code
+`,
+
+  "<br>",
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me a email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to my portfolio! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
